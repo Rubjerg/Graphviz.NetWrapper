85e7cd33ef5a98b4cd34a2b39f1cae90be7b06c1
Chiel ten Brinke <ctenbrinke@gmail.com>
2020-02-14 09:30:56 +0100

    comment

--- TestNugetPackage/NUnitTestProjectWithIndirectDependencies/UnitTest1.cs
+++ TestNugetPackage/NUnitTestProjectWithIndirectDependencies/UnitTest1.cs
@@ -5,6 +5,9 @@ namespace NUnitTestProjectWithIndirectDependencies
     [TestFixture]
     public class Tests
     {
+        /// <summary>
+        /// Test the deployment of the live nuget package by making some basic calls to the graphviz libraries.
+        /// </summary>
         [Test]
         public void TestGraphvizCalls()
         {
