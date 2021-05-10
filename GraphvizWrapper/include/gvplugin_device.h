7c6fc7fa204c8db5d8ff06056cc2331972b70605
Chiel ten Brinke <ctenbrinke@gmail.com>
2021-05-10 15:20:18 +0200

    Add graphviz files for debug mode and upgrade graphviz version (#40)
    * Move graphviz files to Release directory
    
    * Add debug files
    
    * Upgrade release files
    
    * Make MAXSHORT macro compatible with windows headers again.

--- GraphvizWrapper/include/gvplugin_device.h
+++ GraphvizWrapper/include/gvplugin_device.h
@@ -1,6 +1,3 @@
-/* $Id$ $Revision$ */
-/* vim:set shiftwidth=4 ts=8: */
-
 /*************************************************************************
  * Copyright (c) 2011 AT&T Intellectual Property 
  * All rights reserved. This program and the accompanying materials
@@ -8,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: See CVS logs. Details at http://www.graphviz.org/
+ * Contributors: Details at https://graphviz.org
  *************************************************************************/
 
 #ifndef GVDEVICE_PLUGIN_H
