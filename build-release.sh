ba62afe32b63b40fd6139f59a27c4e4aca1dcf58
chtenb <ctenbrinke@gmail.com>
2023-09-06 14:50:48 +0200

    Release 2.0.0

--- build-release.sh
+++ build-release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clean -dfx && nuget restore && msbuild //p:configuration=release
+git clean -dfx && nuget restore Rubjerg.Graphviz.sln && C:\\Program\ Files\\Microsoft\ Visual\ Studio\\2022\\Professional\\Msbuild\\Current\\Bin\\MSBuild.exe //p:configuration=release Rubjerg.Graphviz.sln
