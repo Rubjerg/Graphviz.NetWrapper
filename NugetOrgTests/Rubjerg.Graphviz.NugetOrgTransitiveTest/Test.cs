04c0e591025effa18453f85490ee6d5ee6549864
chtenb <ctenbrinke@gmail.com>
2025-05-27 10:38:32 +0200

    package version logic

--- NugetOrgTests/Rubjerg.Graphviz.NugetOrgTransitiveTest/Test.cs
+++ NugetOrgTests/Rubjerg.Graphviz.NugetOrgTransitiveTest/Test.cs
@@ -1,34 +1,17 @@
-using System.Linq;
 using NUnit.Framework;
+using Rubjerg.Graphviz.Test;
 
 namespace Rubjerg.Graphviz.NugetOrgTest
 {
     [TestFixture()]
-    public class TestNugetPackage
+    public class TestNugetPackageTransitive
     {
         [Test()]
-        public void TestReadDotFile()
+        public void Test()
         {
-            RootGraph root = RootGraph.FromDotString(@"
-digraph test {
-    A;
-    B;
-    B -> B;
-    A -> B[name = edgename];
-    A -> B[name = edgename];
-    A -> B[name = edgename];
-}
-");
-            var A = root.GetNode("A");
-            Assert.AreEqual(3, A.EdgesOut().Count());
-
-            var B = root.GetNode("B");
-            _ = root.GetOrAddEdge(A, B, "");
-            Assert.AreEqual(4, A.EdgesOut().Count());
-
-            root.ToSvgFile(TestContext.CurrentContext.TestDirectory + "/dot_out.svg");
-
-            root.ToSvgFile(TestContext.CurrentContext.TestDirectory + "/neato_out.svg", LayoutEngines.Neato);
+            var test = new TestNugetPackage();
+            test.TestReadDotFile();
         }
     }
 }
+
