f738a41f14851f0b73efd68ae46afa22e8b6f558
Jawahars <jawaharbtech@hotmail.com>
2026-03-09 19:47:38 +0530

    Replace S390x conditional compilation with runtime value
    Uses a numeric cast for the S390x enum value to maintain compatibility with .NET Standard 2.0 while supporting newer .NET 6+ environments.

--- Rubjerg.Graphviz/GraphvizCommand.cs
+++ Rubjerg.Graphviz/GraphvizCommand.cs
@@ -27,9 +27,9 @@ public class GraphvizCommand
                 Architecture.Arm64 => "arm64",
                 Architecture.X86 => "x86",
                 Architecture.Arm => "arm",
-#if NET6_0_OR_GREATER
-                Architecture.S390x => "s390x",  // s390x support added in .NET 6
-#endif
+                // Cast allows compilation in .NET Standard 2.0/2.1.
+                // (Architecture)5 is S390x, added in .NET 6.
+                (Architecture)5 => "s390x",
                 _ => "unknown"
             };
 
